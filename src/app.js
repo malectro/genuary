509ae2a49a93852ada72fd2b4574cb5cb046f592
Kyle Warren <qarren@gmail.com>
2022-01-13 15:04:09 -0800

    added a record button for chrome

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+//import { getOriginPrivateDirectory } from 'https://cdn.jsdelivr.net/npm/native-file-system-adapter/mod.js';
+//const {getOriginPrivateDirectory} = await import('https://cdn.jsdelivr.net/npm/native-file-system-adapter/mod.js');
+
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -34,10 +37,11 @@ function animate(time) {
       let distortion = (xd + Math.abs(y - yCenter)) / (horizon + yCenter);
       distortion = Math.sqrt(xd * xd + yd * yd) / diagonal;
       let d = xd * (1 - distortion * 0.0001);
-      let seed = Math.round((Math.cos(d * d + slowTime) + 1) / 2) * 255;
-      pixels[i + 1] = seed;
-      pixels[i + 0] = Math.round((Math.sin(d * d + slowTime) + 1) / 2) * 255
-      pixels[i + 3] = (1 - distortion) * 255;
+      const alpha = (1 - distortion);
+      pixels[i + 0] = alpha * Math.round((Math.sin(d * d + slowTime) + 1) / 2) * 255;
+      pixels[i + 1] = alpha * Math.round((Math.cos(d * d + slowTime) + 1) / 2) * 255;
+      pixels[i + 3] = 255;
+      //pixels[i + 3] = (1 - distortion) * 255;
     }
   }
 
@@ -45,3 +49,55 @@ function animate(time) {
 }
 
 requestAnimationFrame(animate);
+
+let isRecording = false;
+const recordButton = document.createElement('button');
+recordButton.innerText = 'Record';
+document.body.appendChild(recordButton);
+
+const mediaRecorder = new MediaRecorder(canvas.captureStream(30), {
+  mimeType: 'video/webm',
+});
+
+let chunks = [];
+mediaRecorder.addEventListener('dataavailable', (event) => {
+  chunks.push(event.data);
+});
+
+mediaRecorder.addEventListener('stop', (event) => {
+  const blob = new Blob(chunks, {type: ''});
+  chunks = [];
+
+  window
+    .showSaveFilePicker({
+      suggestedName: 'video',
+      types: [{accept: {'video/webm': ['.webm']}}],
+    })
+    .then(async (fileSystem) => {
+      console.log('hi', fileSystem);
+
+      const stream = await fileSystem.createWritable();
+      stream.write(blob);
+      stream.close();
+    }, () => {
+      // abort
+    });
+});
+
+recordButton.addEventListener('click', () => {
+  if (isRecording) {
+    isRecording = false;
+    mediaRecorder.stop();
+    recordButton.innerText = 'Record';
+  } else {
+    isRecording = true;
+    mediaRecorder.start();
+    recordButton.innerText = 'Stop';
+  }
+});
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
