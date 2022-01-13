209f246f00dbca9bf7768e9768ca8d251f2bf89a
Kyle Warren <qarren@gmail.com>
2022-01-13 15:09:48 -0800

    recorder

--- site/index.d56a3cb1.js
+++ site/index.d56a3cb1.js
@@ -519,6 +519,8 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"5HwUs":[function(require,module,exports) {
+//import { getOriginPrivateDirectory } from 'https://cdn.jsdelivr.net/npm/native-file-system-adapter/mod.js';
+//const {getOriginPrivateDirectory} = await import('https://cdn.jsdelivr.net/npm/native-file-system-adapter/mod.js');
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
@@ -546,14 +548,67 @@ function animate(time) {
         let distortion = (xd + Math.abs(y - yCenter)) / (horizon + yCenter);
         distortion = Math.sqrt(xd * xd + yd * yd) / diagonal;
         let d = xd * (1 - distortion * 0.0001);
-        let seed = Math.round((Math.cos(d * d + slowTime) + 1) / 2) * 255;
-        pixels[i + 1] = seed;
-        pixels[i + 0] = Math.round((Math.sin(d * d + slowTime) + 1) / 2) * 255;
-        pixels[i + 3] = (1 - distortion) * 255;
+        const alpha = 1 - distortion;
+        pixels[i + 0] = alpha * Math.round((Math.sin(d * d + slowTime) + 1) / 2) * 255;
+        pixels[i + 1] = alpha * Math.round((Math.cos(d * d + slowTime) + 1) / 2) * 255;
+        pixels[i + 3] = 255;
+    //pixels[i + 3] = (1 - distortion) * 255;
     }
     ctx.putImageData(imageData, 0, 0);
 }
 requestAnimationFrame(animate);
+let isRecording = false;
+const recordButton = document.createElement('button');
+recordButton.innerText = 'Record';
+document.body.appendChild(recordButton);
+const mediaRecorder = new MediaRecorder(canvas.captureStream(30), {
+    mimeType: 'video/webm'
+});
+let chunks = [];
+mediaRecorder.addEventListener('dataavailable', (event)=>{
+    chunks.push(event.data);
+});
+mediaRecorder.addEventListener('stop', (event)=>{
+    const blob = new Blob(chunks, {
+        type: ''
+    });
+    chunks = [];
+    window.showSaveFilePicker({
+        suggestedName: 'video',
+        types: [
+            {
+                accept: {
+                    'video/webm': [
+                        '.webm'
+                    ]
+                }
+            }
+        ]
+    }).then(async (fileSystem)=>{
+        console.log('hi', fileSystem);
+        const stream = await fileSystem.createWritable();
+        stream.write(blob);
+        stream.close();
+    }, ()=>{
+    // abort
+    });
+});
+recordButton.addEventListener('click', ()=>{
+    if (isRecording) {
+        isRecording = false;
+        mediaRecorder.stop();
+        recordButton.innerText = 'Record';
+    } else {
+        isRecording = true;
+        mediaRecorder.start();
+        recordButton.innerText = 'Stop';
+    }
+});
+function wait(ms) {
+    return new Promise((resolve)=>{
+        setTimeout(resolve, ms);
+    });
+}
 
 },{}]},["3KXv7","5HwUs"], "5HwUs", "parcelRequire94c2")
 
